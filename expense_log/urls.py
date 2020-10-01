3a7d8bfbc7b86c33c730110317eae81dbef71255
dntfury <37257695+dntfury@users.noreply.github.com>
2020-10-01 15:18:02 +0200

    new commit

--- expense_log/urls.py
+++ expense_log/urls.py
@@ -5,7 +5,7 @@ app_name = "expence_log"
 
 urlpatterns = [
     path('create_log/', views.create_log , name = "create_log"),
-    path('update_log/', views.update_log , name = "update_log"),
+    path('update_log/<int:id>', views.update_log , name = "update_log"),
     #path('create_log/', create_log , name = "create_log"),
 
 ]
--- expense_log/views.py
+++ expense_log/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.contrib.auth.decorators import login_required
 #local import
 from .models import ExpenseLog 
 from . import models
 from .forms import ExpenseLogForm
 from django.contrib.auth.models import User
-
+from django.db.models import Q
 # Create your views here.
 
 #Create
@@ -16,7 +16,9 @@ def create_log(request):
         ####expenseLogForm.user = request.user
         if expenseLogForm.is_valid():
             #####
-            expenseLogForm.save()
+            instance = expenseLogForm.save(commit=False)
+            instance.user=request.user
+            instance.save()
         return redirect('dashboard:root')
     else:
         expenseLogForm =ExpenseLogForm()
@@ -30,14 +32,28 @@ def read_log(request):
 
 #update
 @login_required(login_url = 'accounts:authenticate')
-def update_log(request):
+def update_log(request,id):
+    userCheck = Q(user = request.user)
+    expensecheck = Q(pk = id)
+
+    print("trying.................")
+    #user1 = ExpenseLog.objects.get(userCheck)
+    user1 = ExpenseLog.objects.get(userCheck & expensecheck)  
+   
+    #this code still not work corectly and instead of update it just add new row  as expensecheck = Q(pk = id) will be key of table not specific to user may be database need more clearence 
+    #except:
+        #messages.success("Unable to fetch requested user profile")
+    #   return redirect("dashboard:root")
+    
+
     if request.method == 'POST':
-        ####expenseLogForm =ExpenseLogForm(request.POST,instance = user.profile)
+        expenseLogForm =ExpenseLogForm(request.POST,instance = user1)
         if expenseLogForm.is_valid():
-            instance.save()
-        return redirect('dashboard:root')
+            expenseLogForm.save()
+            return redirect('dashboard:root')
     else:
-        ####expenseLogForm =ExpenseLogForm()
+        expenseLogForm =ExpenseLogForm(instance = user1)
+        ##getting multiple instances of expenseLogForm of a single user
         return render(request, "expense_log/update_log.html", {"expenseLogForm":expenseLogForm})
 
 
