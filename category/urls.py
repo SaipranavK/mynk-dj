fc84c670f45b2e23a0b3da3bcfe23df9b6219003
SaipranavK <ksaipranav@gmail.com>
2020-10-01 11:02:46 +0200

    edit and delete implementation for categories module

--- category/urls.py
+++ category/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import add_category
+from .views import add_category, edit_category, delete_confirm_category, delete_category
 
 app_name = "category"
 
 urlpatterns = [
-    path('add/', add_category, name = "add")
+    path('add/', add_category, name = "add"),
+    path('edit/<str:category_name>', edit_category, name = "edit"),
+    path('delete-confirm/<str:category_name>', delete_confirm_category, name = "delete-confirm"),
+    path('delete/<str:category_name>', delete_category, name = "delete")
 ]
--- category/views.py
+++ category/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 from .models import Category
 from .forms import CategoryForm
@@ -34,4 +35,75 @@ def add_category(request):
     return render(request, "category/add.html", {"catForm": catForm})
 
 
+@login_required(login_url = 'accounts:authenticate')
+def edit_category(request, category_name):
 
+        
+    userCheck = Q(user = request.user)
+    categoryCheck = Q(name = category_name)
+
+
+    try:
+        category = Category.objects.get(userCheck & categoryCheck)    
+    
+
+    except:
+        messages.success("Unable to fetch requested category in your profile")
+        return redirect("dashboard:root")
+    
+
+    if request.method == "POST":
+        catForm = CategoryForm(request.POST, instance = category)
+
+        if catForm.is_valid():
+            catForm.save()
+
+            messages.success(request, f"Category Updated")
+            return redirect("dashboard:root")
+        
+    else:
+        catForm = CategoryForm(instance = category)
+
+    return render(request, "category/edit.html", {"catForm": catForm})
+
+    
+@login_required(login_url = 'accounts:authenticate')
+def delete_confirm_category(request, category_name):
+
+    userCheck = Q(user = request.user)
+    categoryCheck = Q(name = category_name)
+
+
+    try:
+        category = Category.objects.get(userCheck & categoryCheck)    
+    
+
+    except:
+        messages.success("Unable to fetch requested category in your profile")
+        return redirect("dashboard:root")
+    
+
+    return render(request, "category/delete-confirm.html", {"category": category})
+
+    
+@login_required(login_url = 'accounts:authenticate')
+def delete_category(request, category_name):
+
+    userCheck = Q(user = request.user)
+    categoryCheck = Q(name = category_name)
+
+
+    try:
+        category = Category.objects.get(userCheck & categoryCheck)    
+    
+
+    except:
+        messages.success("Unable to fetch requested category in your profile")
+        return redirect("dashboard:root")
+
+    category.delete()
+
+    messages.success(request, f"Category deleted from your profile")
+    return redirect("dashboard:root")
+
+    
