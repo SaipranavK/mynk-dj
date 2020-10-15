2fcfa12779a9b70ce7ecb0b22a3f7ecd2a31cf9c
SaipranavK <ksaipranav@gmail.com>
2020-10-15 11:09:42 +0200

    Added unit test for accounts and category

--- category/tests.py
+++ category/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .models import Category
+
+from accounts.models import Profile
+
+import datetime
+
+class CategoryModelTest(TestCase):
+
+    def setUpTestData():
+        user = User.objects.create(username = "testuser", password = "test@12345")
+        profile = Profile.objects.create(user = user, monthly_limit = 10000, monthly_savings = 1000)
+        category = Category.objects.create(user = user, name = "Test Category", value = 3000)
+
+    def test_categoryDB(self):
+        
+        User_record = User.objects.get(username = "testuser")
+        Profile_record = Profile.objects.get(user = User_record)
+        Category_record = Category.objects.get(user = User_record, name = "Test Category")
+       
+        #assertion 
+        self.assertEqual(User_record.username, "testuser")
+        self.assertEqual(Profile_record.user.username, "testuser")
+        self.assertEqual(Category_record.user.username, "testuser")
+        self.assertEqual(Category_record.name, "Test Category")
+        
+
